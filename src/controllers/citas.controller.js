390dedad9dd155fe8df05a8a2d0d08630777e8b1
Erick Taco <ericktaco68@gmail.com>
2023-12-23 00:54:50 -0500

    api v2.7

--- src/controllers/citas.controller.js
+++ src/controllers/citas.controller.js
@@ -2,8 +2,8 @@ import { pool } from "../Db.js";
 import jwt from "jsonwebtoken";
 
 export const citas = async function (req, res) {
-  const events = await pool.query("select title from tablacitas");
-  res.json(events);
+  const { events } = await pool.query("select title from tablacitas");
+  res.json({ events });
 };
 
 export const clientes = async function (req, res) {
