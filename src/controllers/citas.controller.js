d2b6d35bb395596abe33e4e84c0ecc32b0002ead
Erick Taco <ericktaco68@gmail.com>
2024-01-23 22:55:20 -0500

    j

--- src/controllers/citas.controller.js
+++ src/controllers/citas.controller.js
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 import { transporter } from "../email.js";
 
 export const citas = async function (req, res) {
-  const [events] = await pool.query("select * from tablacitas");
-  ("SELECT tablaclientes.Nombre, tablaclientes.Apellido,empleados.Nombre,empleados.Apellido,tablaservicios.NombreServicio,tablahorarios.hora,date FROM tablacitas INNER JOIN tablaclientes on tablacitas.ClienteID =tablaclientes.ClienteID INNER JOIN empleados on tablacitas.EmpleadoID=empleados.EmpleadoID INNER JOIN tablaservicios on tablacitas.servicioSolicitado=tablaservicios.ServicioID INNER JOIN tablahorarios on tablacitas.horaCita=tablahorarios.idHorario");
+  const [events] = await pool.query(
+    "SELECT tablaclientes.Nombre, tablaclientes.Apellido,empleados.Nombre,empleados.Apellido,tablaservicios.NombreServicio,tablahorarios.hora,date FROM tablacitas INNER JOIN tablaclientes on tablacitas.ClienteID =tablaclientes.ClienteID INNER JOIN empleados on tablacitas.EmpleadoID=empleados.EmpleadoID INNER JOIN tablaservicios on tablacitas.servicioSolicitado=tablaservicios.ServicioID INNER JOIN tablahorarios on tablacitas.horaCita=tablahorarios.idHorario"
+  );
   res.json(events);
 };
 
